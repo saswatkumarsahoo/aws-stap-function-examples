993cb7e221dd350cea12ad3b279fa693b5368312
Saswat Sahoo <saswat.kumar.sahoo@randstadgroep.nl>
2023-04-28 17:19:10 +0200

    rename

--- aws_step_function_examples/sfn_distributed_map.py
+++ aws_step_function_examples/sfn_distributed_map.py
@@ -3,7 +3,7 @@ from constructs import Construct
 import json
 
 
-class AwsStepFunctionExamplesStack(Stack):
+class SfnDistributedMap(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
